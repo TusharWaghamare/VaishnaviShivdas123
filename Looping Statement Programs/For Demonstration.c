867e0f3847ac09e2d5f27e14056f934cceb595a1
VaishnaviShivdas123 <93471265+VaishnaviShivdas123@users.noreply.github.com>
2021-11-11 23:17:04 +0530

    Programs Of Looping Statements
    Using (while, for, do-while )

---
+++ Looping Statement Programs/For Demonstration.c
@@ -0,0 +1,20 @@
+//For demonstration.c
+
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i=0;
+
+    for(i=0; i<5;i++)
+    {
+        printf("\n INDIA");
+    }
+    printf("Thanks");
+
+    getch();
+    return 0;
+
+}
---
+++ Looping Statement Programs/do - while demonstraction.c
@@ -0,0 +1,18 @@
+//do - while demonstration.
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i=10;
+    do
+    {
+        printf("\n INDIA");
+        printf("\n value of varibale=%d",i);
+        i++;
+    }while(i<=5);
+    printf("\n Thanks");
+
+    getch();
+    return 0;
+}
