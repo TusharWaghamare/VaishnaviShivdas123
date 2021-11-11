6bb3cdfee314c6710dbe1b91ae96e838b2ecaf07
VaishnaviShivdas123 <93471265+VaishnaviShivdas123@users.noreply.github.com>
2021-11-11 23:25:35 +0530

    Programs Based On Factorial and Fabonacci Series

---
+++ Programs (Factorial and Fabonacci Series )/1.Program To Print Fibonacci Series Using While Loop.c
@@ -0,0 +1,26 @@
+//1.Program To Print Fibonacci Series Using While Loop.c
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int No1 = 1, No2 = 0, No3 = 0, Cnt = 0;
+
+    printf("\n Enter Count For Fibonacci Series = ");
+    scanf("%d", &Cnt);
+
+    printf("\n Fibonacci Series Of Given count = ");
+
+    while(Cnt > 0)
+    {
+        printf("\t%d", No3);
+        No3 = No1 + No2;
+        No1 = No2;
+        No2 = No3;
+        Cnt--;
+    }
+    getch();
+    return 0;
+}
